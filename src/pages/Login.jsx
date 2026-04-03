f4bc744984c0140c8bd743b7543cdbd2c08ba970
akiakash <akiagash12@gmail.com>
2026-04-04 01:28:31 +0530

    cghanges

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "../assets/styles/style.css";
-import logo from "../assets/img/29y_logo.png";
+import logo from "../assets/img/logo.png";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
